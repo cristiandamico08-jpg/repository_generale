31d0bfc99034e97908c41955d5a7e80717a90b52
Cristian Dylan D'Amico <cristian.damico.08@marconigorgonzola.it>
2025-11-27 14:30:51 +0100

    duplicazione cartella per nuovo esercizio

---
+++ Informatica/Sim_verifica_28_nov_ArrayList/Articolo.java
@@ -0,0 +1,38 @@
+package repository_generale.Informatica.Sim_verifica_28_nov;
+
+public class Articolo {
+
+    private String nome;
+    private double prezzo;
+    private final double IVA = 0.22;
+
+
+    public Articolo(String nome, double prezzo) {
+        this.nome = nome;
+        this.prezzo = prezzo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public double getIVA() {
+        return IVA;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public double calcolaPrezzoConIVA() {
+        return prezzo + (prezzo * IVA);
+    }
+}
---
+++ Informatica/Sim_verifica_28_nov_ArrayList/Main.java
@@ -0,0 +1,110 @@
+package repository_generale.Informatica.Sim_verifica_28_nov;
+import java.util.Scanner;
+
+public class Main {
+
+    private Scanner input = new Scanner(System.in);
+    private final int dim = 3;
+    private Articolo[] panini = new Articolo[dim];
+    private Articolo[] bibite = new Articolo[dim];
+    private int scelta;
+    private double totale;
+    private double arrotondamento;
+    private Articolo temp = new Articolo(null, 0);
+
+    public Main(){
+
+        panini[0] = new Articolo("Prosciutto cotto", 2.50);
+        panini[1] = new Articolo("Salame", 3.00);
+        panini[2] = new Articolo("Cotoletta", 2.00);
+
+        bibite[0] = new Articolo("Acqua", 1.00);
+        bibite[1] = new Articolo("Coca cola", 1.50);
+        bibite[2] = new Articolo("Fanta", 1.50);
+
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1. Visualizza panini e compra");
+            System.out.println("2. Visualizza bibite e compra");
+            System.out.println("3. Visualizza scontrino ed esci");
+            System.out.print("Scegli un'opzione: ");
+            scelta = input.nextInt();
+            switch (scelta) {
+                case 1:
+
+                    System.out.println("\nElenco Panini:");
+                    for (int i = 0; i < dim-1; i++) {
+                        for (int j = 0; j < dim-1; j++) {
+                            if (panini[j].getPrezzo() > panini[j+1].getPrezzo()) {
+                                temp = panini[j];
+                                panini[j] = panini[j+1];
+                                panini[j+1] = temp;
+                            }
+                        }
+                    }
+                    for (int i = 0; i < 3; i++) {
+                        System.out.println((i + 1) + ". " + panini[i].getNome() + " - Prezzo: " + panini[i].getPrezzo() + " Euro - Prezzo con IVA: " + panini[i].calcolaPrezzoConIVA() + " Euro");
+                    }
+
+                    do {
+                        System.out.println("\n\nSeleziona il numero del panino che vuoi comprare");
+                        int scelta = input.nextInt();
+                        if (scelta >= 1 && scelta <= dim) {
+                            System.out.println("Hai comprato il panino: " + panini[scelta - 1].getNome() + " al prezzo di " + panini[scelta - 1].calcolaPrezzoConIVA() + " Euro (IVA inclusa)");
+                            totale = totale + panini[scelta - 1].calcolaPrezzoConIVA();
+                            arrotondamento = Math.round(totale * 100.0) / 100.0;
+                            totale = arrotondamento;
+                            break;
+                        } else {
+                            System.out.println("\n\n\nScelta non valida, riprova.");
+                        }
+                    } while (true);
+
+                    break;
+
+                case 2:
+
+                    System.out.println("\nElenco Bibite:");
+                    for (int i = 0; i < dim-1; i++) {
+                        for (int j = 0; j < dim-1; j++) {
+                            if (panini[j].getPrezzo() > panini[j+1].getPrezzo()) {
+                                temp = panini[j];
+                                panini[j] = panini[j+1];
+                                panini[j+1] = temp;
+                            }
+                        }
+                    }
+                    for (int i = 0; i < dim; i++) {
+                        System.out.println((i + 1) + ". " + bibite[i].getNome() + " - Prezzo: " + bibite[i].getPrezzo() + " Euro - Prezzo con IVA: " + bibite[i].calcolaPrezzoConIVA() + " Euro");
+                    }
+                    do {
+                        System.out.println("\n\nSeleziona il numero della bibita che vuoi comprare");
+                        scelta = input.nextInt();
+                        if (scelta >= 1 && scelta <= dim) {
+                            System.out.println("Hai comprato la bibita: " + bibite[scelta - 1].getNome() + " al prezzo di " + bibite[scelta - 1].calcolaPrezzoConIVA() + " Euro (IVA inclusa)");
+                            totale = totale + bibite[scelta - 1].calcolaPrezzoConIVA();
+                            arrotondamento = Math.round(totale * 100.0) / 100.0;
+                            totale = arrotondamento;
+                            break;
+                        } else {
+                            System.out.println("\n\n\nScelta non valida, riprova.");
+                        }
+                    } while (true);
+
+                    break;
+
+                case 3:
+                    System.out.println("\nIl totale del tuo scontrino è di: " + totale + " euro");
+                    return;
+
+                default:
+                    System.out.println("\n\n\n"  + scelta + " non è un'opzione valida.");
+                    break;
+            }
+        } while (true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
